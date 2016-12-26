d7da67733bbb699c983e1b07ba1edbb0d72375ee
Gal Bar-Nissan <gal.barnissan@gmail.com>
2016-12-26 17:25:14 +0200

    spinner component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {GreetInput} from './components/GreetInput/greetInput';
 import {Greeter} from './components/Greeter/greeter';
 import DropboxTeam from 'dropbox/src/team';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CircularProgress from 'material-ui/CircularProgress';  
 
 // const App = () => <h1>Hello world!</h1>
 
@@ -9,8 +11,9 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
+      fetchingState: "INIT",
       teamToken: "",
-      greeting: "world"
+      greeting: null,
     }
   }
 
@@ -20,20 +23,37 @@ class App extends React.Component{
 
   getTeamName(teamToken) {   
    const dbx = new DropboxTeam({ accessToken:  teamToken});
+   this.setState({fetchingState: "FETCHING"})
    dbx.teamGetInfo()
     .then(function(response) {
-       this.setState({greeting: response.name});
+        this.setState({greeting: response.name});
+        this.setState({fetchingState: "SUCCESS"});       
    }.bind(this));
+  }
+
+  renderGreet() {
+    if(this.state.fetchingState === "SUCCESS"){
+      return <Greeter greeting={this.state.greeting}/>
+    }
+    else if(this.state.fetchingState === "FETCHING"){
+      return <CircularProgress size={60} thickness={7} />
+    }
   }
 
   render () {
     return (
       <div>
-        <GreetInput update={this.update.bind(this)}/>
-        <Greeter greeting={this.state.greeting}/>
+        <GreetInput update={this.update.bind(this)}/>        
+        {this.renderGreet()}
       </div>
     )
   }
 }
 
-export default App
+  const MuiApp = () => (                                                                             
+      <MuiThemeProvider>                                                                               
+        <App />                                                                                        
+    </MuiThemeProvider>                                                                              
+  );                                                                                                 
+  
+  export default MuiApp;    
